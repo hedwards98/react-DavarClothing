fc4b5693896a8d49c9e543dd724cdbabfc1e2aac
hedwards98 <henry.edwards@mckesson.com>
2020-05-11 15:27:08 -0400

    lesson 69 - router and navigation setup

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -11,7 +11,7 @@ class Directory extends React.Component {
                 title: 'HATS',
                 imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                 id: 1,
-                linkUrl: 'shop/hats'
+                linkUrl: 'hats'
             },
             {
                 title: 'JACKETS',
@@ -46,12 +46,10 @@ class Directory extends React.Component {
         return(
             <div className='directory-menu'>
                 {
-                    this.state.sections.map(({title, imageUrl, id, size }) => (
+                    this.state.sections.map(({id, ...otherSectionProps }) => (
                         <MenuItem 
                             key={id} 
-                            title={title} 
-                            imageUrl={imageUrl} 
-                            size={size} 
+                            {...otherSectionProps}
                         />
                     ))
                 }
